f9688f42f44e4d1a1a69d38bff42898be4d64b7d
AsianMagic <45053539+AsianMagic@users.noreply.github.com>
2020-02-20 15:56:17 -0800

    Add files via upload

---
+++ Assets/fourScriot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fourScriot : MonoBehaviour
+{
+    public void LoadStartLevel()
+    {
+        Application.LoadLevel("meh");
+    }
+}
---
+++ Assets/twoScriot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class twoScriot : MonoBehaviour
+{
+    public void LoadStartLevel()
+    {
+        Application.LoadLevel("Idunno");
+    }
+}
